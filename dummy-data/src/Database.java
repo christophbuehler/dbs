96360324c434a9428356456f69a45fd3cb153dd7
Christoph Bühler <christoph@cbuehler.li>
2021-01-28 03:57:10 +0100

    create dummy data

--- dummy-data/src/Database.java
+++ dummy-data/src/Database.java
@@ -1,44 +1,104 @@
 import java.sql.*;
+import java.util.Locale;
+
+import com.github.javafaker.Faker;
 import oracle.jdbc.driver.*;
 
 public class Database {
-	private String connStr;
-	private String uname;
-	private String pwd;
+	private Env env;
 	private boolean isConnected = false;
 	private Connection con;
-	
-	public Database(String connStr, String uname, String pwd) {
-		this.connStr = connStr;
-		this.uname = uname;
-		this.pwd = pwd;
+
+	private final int PERSON_COUNT = 10000;
+	private String[][] persons = new String[PERSON_COUNT][2]; // nachname, vorname
+	private String[] unis = { "Univie", "WU", "TU" };
+	private String[] tables = {
+		"autor_hat_post",
+		"autor_hat_sprache",
+		"revision",
+		"post",
+		"autor",
+		"mitarbeiter",
+		"person",
+		"universitaet",
+		"sprache"
+	};
+
+	public Database(Env env) {
+		this.env = env;
+		genFakeData();
 	}
-	
-	public void connect() throws ClassNotFoundException, SQLException {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		// String dburl = "jdbc:oracle:thin:@oracle-lab.cs.univie.ac.at:1521:lab";
-		// String dburl = "jdbc:oracle:thin:@vier.cs.univie.ac.at:1521:o10g ";
-		this.con = DriverManager.getConnection(this.connStr, this.uname, this.pwd);
-		this.isConnected = true;
+
+	public void removeAll() throws SQLException {
+		for(String table : tables) {
+			Statement stmt = con.createStatement();
+			String sql = "DELETE FROM " + table;
+			stmt.executeUpdate(sql);
+		}
 	}
-	
-	public void disconnect() throws Exception {
-		this.con.close();
-	}
-	
+
 	public void insertAll() throws Exception {
-		if (!this.isConnected) throw new Exception("Client not connected.");
-		this.insertSprachen();
+		if (!isConnected)
+			throw new Exception("Client not connected.");
+		insertSprachen();
+		insertUnis();
+
+		insertPersonen();
+
+		insertAutorHatArtikel();
+	}
+
+	public void insertPersonen() throws SQLException {
+		PreparedStatement stmt = con.prepareStatement("INSERT INTO person (vorname, nachname) VALUES (?, ?)");
+		con.setAutoCommit(false);
+
+		for(String[] person : persons) {
+			stmt.setString(1, person[0]);
+			stmt.setString(2, person[1]);
+			stmt.executeUpdate();
+			con.commit();
+		}
 	}
 
 	public void insertSprachen() throws SQLException {
 		Statement stmt = con.createStatement();
 		String sql = "INSERT INTO sprache VALUES ('de', 0, 'deutsch', 'UTF-8')";
 		stmt.executeUpdate(sql);
+	}
+
+	public void insertUnis() throws SQLException {
+		PreparedStatement stmt = con.prepareStatement("INSERT INTO universitaet (name, studierende) VALUES (?, 0)");
+		con.setAutoCommit(false);
 		
-		/*ResultSet rs = stmt.executeQuery(sql);
-		while (rs.next()) {
-			String konr = rs.getString("konr");
-			System.out.println(konr + " " + rs.getInt("kontostand"));*/
+		for(String uni : unis) {
+			stmt.setString(1, uni);
+			stmt.executeUpdate();
+			con.commit();
+		}
+	}
+
+	public void insertAutorHatArtikel() throws SQLException { }
+
+	public void connect() throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		con = DriverManager.getConnection(env.get("conn_str"), env.get("uname"), env.get("pwd"));
+		isConnected = true;
+	}
+
+	public void disconnect() throws Exception {
+		con.close();
+		isConnected = false;
+	}
+	
+	private void genFakeData() {
+		Faker faker = new Faker(new Locale("de-DE"));
+
+		for (int i=0; i<PERSON_COUNT; ++i) {
+			persons[i] = new String[] {
+				faker.name().firstName(),
+				faker.name().lastName(),
+			};
+		}
+
 	}
 }
--- dummy-data/src/Main.java
+++ dummy-data/src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
 	public static void main(String[] args) {
-		Database db = new Database();
+		Env env = new Env("../univie.env");
+		Database db = new Database(env);
+
 		try {
 			db.connect();
+			db.removeAll();
 			db.insertAll();
 			db.disconnect();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
