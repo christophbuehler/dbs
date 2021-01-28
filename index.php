f9bf3bd113f4f1686df4e9c88d13ee259e0e56bd
Christoph Bühler <christoph@cbuehler.li>
2021-01-28 01:29:59 +0100

    index

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<?php
-
-
-$postData = json_encode($_POST);
-$_POST = json_decode(Utils::utf8_urldecode(
-
-  // replace plus sign
-  str_replace('+', '%2B', $postData)
-), true);
-
-$this->reqArgs = array_merge($_POST, $_GET);
-
+HELLO :P
