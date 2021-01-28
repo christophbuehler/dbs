4c017e46fb5d2204c2049af0743bc9e6bd894eb2
Christoph Bühler <christoph@cbuehler.li>
2021-01-28 12:47:59 +0100

    routing without htaccess rewrite (it is disabled)

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
-$method = $_SERVER['REQUEST_METHOD'];
-echo $method;
+
+handle('GET', '/posts/univie', function () {
+  echo '/posts/univie';
+});
 
 // [GET] /api/posts/univie
+
+function handle($_method, $_rif, $callback) {
+  $method_correct = strtoupper($_method) == strtoupper($_SERVER['REQUEST_METHOD']);
+  if (!$method_correct) return;
+  $rif = $_GET['rif'];
+  $rif_correct = strtoupper($_rif) == strtoupper($rif);
+  if (!$rif_correct) return;
+  $callback();
+  exit;
+}
 
 ?>
 
 
 
-
-HELLO AGAIN :P 4
 
 
 lorem
